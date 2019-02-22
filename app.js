32bc9c8b97135aa40bb8924af80f62e842c0cbc4
barrard <barrard@gmail.com>
2019-02-21 19:58:55 -1000

    turns on and off the camera

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ socketServer.on("connection", function(socket, upgradeReq) {
   if (socketServer.connectionCount == 0 && !WEBCAM_PROCEES) {
     //Start the webcam process
     console.log("Starting webcam!!");
-    const ffmpeg_command = `ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 http://localhost:8181/supersecret`;
+    const ffmpeg_command = `ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 http://localhost:8181/`;
     WEBCAM_PROCEES = cmd.run(ffmpeg_command, cmd_callback);
     // const pid = WEBCAM_PROCEES.pid
     // console.log({WEBCAM_PROCEES_ID})
