ffd24d4fe0bd8f74666c6317413dd1f028afc5b3
barrard <barrard@gmail.com>
2019-02-21 14:59:00 -1000

    changed streamer to webcam

--- app.js
+++ app.js
@@ -12,95 +12,113 @@
 //   })
 // })
 
-var fs = require('fs')
+var fs = require("fs");
 
-var http = require('http')
+var http = require("http");
 
-var WebSocket = require('ws')
-var express = require('express')
-var app = express()
-app.enable('trust proxy');
+var WebSocket = require("ws");
+var express = require("express");
+var app = express();
+app.enable("trust proxy");
 
 if (process.argv.length < 3) {
   console.log(
-    'Usage: \n' +
-		'node websocket-relay.js <secret> [<stream-port> <websocket-port>]'
-  )
-  process.exit()
+    "Usage: \n" +
+      "node websocket-relay.js <secret> [<stream-port> <websocket-port>]"
+  );
+  process.exit();
 }
 
-var STREAM_SECRET = process.argv[2]
+var STREAM_SECRET = process.argv[2];
 
-var STREAM_PORT = process.argv[3] || 8181
+var STREAM_PORT = process.argv[3] || 8181;
 
-var WEBSOCKET_PORT = process.argv[4] || 8182
+var WEBSOCKET_PORT = process.argv[4] || 8182;
 
-var RECORD_STREAM = false
+var RECORD_STREAM = false;
 
 // Websocket Server
 var socketServer = new WebSocket.Server({
   port: WEBSOCKET_PORT,
   perMessageDeflate: false
-})
-socketServer.connectionCount = 0
-socketServer.on('connection', function (socket, upgradeReq) {
-  socketServer.connectionCount++
+});
+socketServer.connectionCount = 0;
+socketServer.on("connection", function(socket, upgradeReq) {
+  socketServer.connectionCount++;
   console.log(
-    'New WebSocket Connection: ',
+    "New WebSocket Connection: ",
     (upgradeReq || socket.upgradeReq).socket.remoteAddress,
-    (upgradeReq || socket.upgradeReq).headers['user-agent'],
-    '(' + socketServer.connectionCount + ' total)'
-  )
-  socket.on('close', function (code, message) {
-    socketServer.connectionCount--
+    (upgradeReq || socket.upgradeReq).headers["user-agent"],
+    "(" + socketServer.connectionCount + " total)"
+  );
+  socket.on("close", function(code, message) {
+    socketServer.connectionCount--;
     console.log(
-      'Disconnected WebSocket (' + socketServer.connectionCount + ' total)'
-    )
-  })
-})
-socketServer.broadcast = function (data) {
-  socketServer.clients.forEach(function (client) {
+      "Disconnected WebSocket (" + socketServer.connectionCount + " total)"
+    );
+  });
+});
+socketServer.broadcast = function(data) {
+  socketServer.clients.forEach(function(client) {
+    console.log(client)
     if (client.readyState === WebSocket.OPEN) {
-      client.send(data)
+      console.log('sending Data')
+      console.log(data)
+      client.send(data);
     }
-  })
-}
+  });
+};
 
 // HTTP Server to accept incomming MPEG-TS Stream from ffmpeg
-app.get('*', (request, response) => {
-  var params = request.url.substr(1).split('/')
-  console.log(params)
+app
+  .get("*", (request, response) => {
+    var params = request.url.substr(1).split("/");
+    console.log(params);
 
-  if (params[0] !== STREAM_SECRET) {
+    if (params[0] !== STREAM_SECRET) {
+      console.log(
+        "Failed Stream Connection: " +
+          request.socket.remoteAddress +
+          ":" +
+          request.socket.remotePort +
+          " - wrong secret."
+      );
+      response.end();
+    }
+
+    response.connection.setTimeout(0);
     console.log(
-      'Failed Stream Connection: ' + request.socket.remoteAddress + ':' + request.socket.remotePort + ' - wrong secret.'
-    )
-    response.end()
-  }
+      "Stream Connected: " +
+        request.socket.remoteAddress +
+        ":" +
+        request.socket.remotePort
+    );
+    request.on("data", function(data) {
+      socketServer.broadcast(data);
+      if (request.socket.recording) {
+        request.socket.recording.write(data);
+      }
+    });
+    request.on("end", function() {
+      console.log("close");
+      if (request.socket.recording) {
+        request.socket.recording.close();
+      }
+    });
 
-  response.connection.setTimeout(0)
-  console.log(
-    'Stream Connected: ' + request.socket.remoteAddress + ':' + 		request.socket.remotePort
-  )
-  request.on('data', function (data) {
-    socketServer.broadcast(data)
-    if (request.socket.recording) {
-      request.socket.recording.write(data)
+    // Record the stream to a local file?
+    if (RECORD_STREAM) {
+      var path = "recordings/" + Date.now() + ".ts";
+      request.socket.recording = fs.createWriteStream(path);
     }
   })
-  request.on('end', function () {
-    console.log('close')
-    if (request.socket.recording) {
-      request.socket.recording.close()
-    }
-  })
+  .listen(STREAM_PORT);
 
-  // Record the stream to a local file?
-  if (RECORD_STREAM) {
-    var path = 'recordings/' + Date.now() + '.ts'
-    request.socket.recording = fs.createWriteStream(path)
-  }
-}).listen(STREAM_PORT)
-
-console.log('Listening for incomming MPEG-TS Stream on http://127.0.0.1:' + STREAM_PORT + '/<secret>')
-console.log('Awaiting WebSocket connections on ws://127.0.0.1:' + WEBSOCKET_PORT + '/')
+console.log(
+  "Listening for incomming MPEG-TS Stream on http://127.0.0.1:" +
+    STREAM_PORT +
+    "/<secret>"
+);
+console.log(
+  "Awaiting WebSocket connections on ws://127.0.0.1:" + WEBSOCKET_PORT + "/"
+);
